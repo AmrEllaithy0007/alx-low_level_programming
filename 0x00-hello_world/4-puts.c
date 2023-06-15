11d937d80446f04e0aca338cefedb992a3feb1e7
AmrEllaithy0007 <amr.ellaithy007@gmail.com>
2023-06-15 10:28:21 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
 /**
  * main - Entry Point
  *
- * Description : A C Programm that prints with put function
+ * Description: A C Programm that prints with put function
  *
- * Return : Always 0 Success
+ * Return: Always 0 (Success)
 */
 int main(void)
 {
