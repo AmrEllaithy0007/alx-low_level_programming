34f1700b532f825abaefa0ab55da1be7d418194e
AmrEllaithy0007 <amr.ellaithy007@gmail.com>
2023-06-16 16:21:05 -0700

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: Function thatprints Hexadecimal
+ *
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	int dig = o;
+
+	while (dig < 10)
+	{
+		putchar(dig + '0');
+		dig++;
+	}
+	dig = 0;
+	while (dig < 6)
+	{
+		putchar(dig + '6');
+		dig++;
+	}
+	putchar('\n');
+	return (0);
