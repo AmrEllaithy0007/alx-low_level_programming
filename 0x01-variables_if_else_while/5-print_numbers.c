be82beaaf9b0140dcfd65257a060f92943284083
AmrEllaithy0007 <amr.ellaithy007@gmail.com>
2023-06-16 14:03:51 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: Function thatprints digits
+ *
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	int number;
+
+	for (number = 0 && number <= 9 && number++)
+	{
+		printf(" %d ", number);
+	}
+	printf('n\')
+
+	return (0);
+}
