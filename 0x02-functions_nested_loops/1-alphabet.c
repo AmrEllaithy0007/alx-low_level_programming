15be0bf92dec9b4ebca514f9111e7ee71513d3ae
AmrEllaithy0007 <amr.ellaithy007@gmail.com>
2023-06-20 09:16:36 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_alphabet - printing alphabet
+ *
+ *Return: Always (0) (Success)
+ **/
+void print_alphabet(void)
+{
+	char = c;
+	for (c = 'a'; a <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+		_putchar('\n');
+}
