3fc05de343dae30997a7199ce8e1f3f622100cc1
AmrEllaithy0007 <amr.ellaithy007@gmail.com>
2023-06-20 17:00:56 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ *@num1: the first number to be computed
+ *@num2: the second number to be computed
+ *
+ *Return: Int Values
+ **/
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
