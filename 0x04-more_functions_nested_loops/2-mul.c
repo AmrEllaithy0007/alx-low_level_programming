aea1dae1b837d1e08d61ae715c51749dcfeab8ab
AmrEllaithy0007 <amr.ellaithy007@gmail.com>
2023-06-22 13:37:45 -0700

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *mul - Entry point
+ *
+ *@a: character need to be computed
+ *@b: character need to be computed
+ *
+ *Return: mult of to integr
+ */
+
+int mul(int a, int b)
+{
+	int mult ;
+
+	mult = a * b ;
+	return (mult);
+}
