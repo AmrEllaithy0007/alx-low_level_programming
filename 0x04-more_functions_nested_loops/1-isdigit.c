0493bdaa5cadf243f450b9eb71774d7cf56e2b10
AmrEllaithy0007 <amr.ellaithy007@gmail.com>
2023-06-22 13:00:47 -0700

    1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_isdigit - Entry point
+ *
+ *@c: Character need computed
+ *
+ *Return: 1 if digit
+ **/
+
+int _isdigit(int c)
+
+{
+	if (c >= 9)
+		return (1);
+	else
+		return (0);
+}
