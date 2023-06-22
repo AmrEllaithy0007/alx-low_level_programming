abee8ff29720bc609144ea5a7c8219c1d64444f0
AmrEllaithy0007 <amr.ellaithy007@gmail.com>
2023-06-22 16:35:55 -0700

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - Entry Point
+ *@n: character need to be computed
+ */
+void print_line(int n)
+{
+	int co;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (co = 1; co <= n; co++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
