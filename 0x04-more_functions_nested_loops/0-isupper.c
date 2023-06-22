6db189a1f995361c7482f8e3e50ea3f9496690c9
AmrEllaithy0007 <amr.ellaithy007@gmail.com>
2023-06-22 12:46:37 -0700

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - Entry point
+ *
+ *@c: character need identify
+ *
+ * Return: 1 if upper 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	char ;
+
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
