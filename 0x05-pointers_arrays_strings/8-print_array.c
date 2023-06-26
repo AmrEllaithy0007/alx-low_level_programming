43cb5e0c179840026e8f91829fb98f7c07c5149d
AmrEllaithy0007 <amr.ellaithy007@gmail.com>
2023-06-26 16:24:06 -0700

    8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_array - Main Entry
+ *@n: numbers of Integr
+ *@a: array of Integr
+ *Return: Void
+ *
+ **/
+
+void print_array(int *a, int n)
+{
+int = j;
+
+for (j = 0; j < n; j++)
+{
+printf("%d", a[j]);
+if (j != (n - 1))
+printf(", ");
+}
+}
+printf("\n");
+}
