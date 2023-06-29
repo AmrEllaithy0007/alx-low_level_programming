afef989d22067c4a806fee55b7b4cb3cc63fe0c0
AmrEllaithy0007 <amr.ellaithy007@gmail.com>
2023-06-29 13:21:21 -0700

    100

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,3 +9,4 @@ char *_strncpy(char *dest, char *src, int n);
 char *string_toupper(char *str);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
