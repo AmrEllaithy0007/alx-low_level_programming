c6d6581ac61d1d7fa0394e9d20b4badeaa2d4139
AmrEllaithy0007 <amr.ellaithy007@gmail.com>
2023-07-03 16:15:51 -0700

    8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,12 +1,10 @@
 #include "main.h"
-#include  <stdio.h>
+#include <stdio.h>
 /**
- *print_diagsums - Entry point
- *@a: integr
- *@size: integr
- *
- *Return: Always 0 (Success)
- **/
+ * print_diagsums - Print diagonal sum.
+ * @a: an array of pointers of integers
+ * @size: array size
+ */
 void print_diagsums(int *a, int size)
 {
 	int i, j, k, d1, d2;
@@ -15,7 +13,6 @@ void print_diagsums(int *a, int size)
 	d1 = 0;
 	d2 = 0;
 	for (i = 0; i < size; i++)
-	{
 		for (j = 0; j < size; j++)
 		{
 			(i == j) ? d1 += a[k] : 0;
@@ -23,5 +20,4 @@ void print_diagsums(int *a, int size)
 			k++;
 		}
 	printf("%i, %i\n", d1, d2);
-	}
 }
