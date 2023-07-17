dd97a45b4ce51b0b33bf871465b83df25254a584
AmrEllaithy0007 <amr.ellaithy007@gmail.com>
2023-07-17 06:50:18 -0700

    2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *description:function returns  file name
+ *Return: Always 0 (Success)
+ *
+ **/
+int main(void)
+{
+
+	printf("%s\n", __FILE__);
+	return (0);
+}
