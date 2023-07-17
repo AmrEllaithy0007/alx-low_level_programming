ea3be85b06e7a1ed091621e6df22b2ed5c9367e1
AmrEllaithy0007 <amr.ellaithy007@gmail.com>
2023-07-17 09:06:23 -0700

    1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,25 @@
-#ifndef _dog_h_
-#define _dog_h_
+#ifndef STRUCTS
+#define STRUCTS
 /**
- *struct dog - Entry point
- *@name: char
- *@age: float
- *@owner: char
- *description: hot dog
- **/
-struct dog
+ * struct dog - description for a pet
+ * @name: pet name
+ * @age: pet age
+ * @owner: owner
+ *
+ * Description: Longer description
+ */
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+#endif
+#ifndef _FUNCTIONS_H
+#define _FUNCTIONS_H
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
