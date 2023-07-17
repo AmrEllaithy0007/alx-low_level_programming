272d70136effbcc7fb5eba9e91a5b659c76391a0
AmrEllaithy0007 <amr.ellaithy007@gmail.com>
2023-07-17 08:19:06 -0700

    hotdog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _dog_h_
+#define _dog_h_
+/**
+ *struct dog - Entry point
+ *@name: char
+ *@age: float
+ *@owner: char
+ *description: hot dog
+ **/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
